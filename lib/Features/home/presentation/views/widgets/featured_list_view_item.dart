72d86b577516b06b1e7e2b32373428307e55aa8d
Saleh Yossef <108934387+Salehy900@users.noreply.github.com>
2024-09-25 12:03:36 +0300

    create custom book details app bar

--- lib/Features/home/presentation/views/widgets/featured_list_view_item.dart
+++ lib/Features/home/presentation/views/widgets/featured_list_view_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:my_bookly/core/utils/app_router.dart';
 import 'package:my_bookly/core/utils/assets.dart';
 
 class FeaturedListViewItem extends StatelessWidget {
@@ -6,15 +8,20 @@ class FeaturedListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.7 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          color: Colors.red,
-          image: const DecorationImage(
-            fit: BoxFit.fill,
-            image: AssetImage(AssetsData.testImage),
+    return GestureDetector(
+      onTap: () {
+        GoRouter.of(context).push(AppRouter.kBookDetails);
+      },
+      child: AspectRatio(
+        aspectRatio: 2.7 / 4,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: Colors.red,
+            image: const DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage(AssetsData.testImage),
+            ),
           ),
         ),
       ),
--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,9 +1,11 @@
 import 'package:go_router/go_router.dart';
+import 'package:my_bookly/Features/home/presentation/views/book_details_view.dart';
 import 'package:my_bookly/Features/home/presentation/views/home_view.dart';
 import 'package:my_bookly/Features/splash/presentation/views/splash_view.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/homeView';
+  static const kBookDetails = '/bookDetails';
   static final GoRouter router = GoRouter(
     routes: [
       GoRoute(
@@ -13,6 +15,10 @@ abstract class AppRouter {
       GoRoute(
         path: kHomeView,
         builder: (context, state) => const HomeView(),
+      ),
+      GoRoute(
+        path: kBookDetails,
+        builder: (context, state) => const BookDetailsView(),
       ),
     ],
   );
